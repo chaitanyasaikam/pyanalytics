ad609347e1241c802e6f93216cde66626c1c92b2
Chaitanya Saikam <chaitanyasaikam@gmail.com>
2019-08-24 03:48:41 +0530

    hands on 1

---
+++ Denco Corp/Denco corp.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+df=pd.read_csv("/Users/KC/Desktop/Henry Harvin/pywork/pyprojects/pyanalytics/Denco Corp/denco.csv")
+df
+df.size
+df.shape
+df.describe
+df.columns
+
